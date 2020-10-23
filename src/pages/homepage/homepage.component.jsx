71bfda01fe733399d1ed3c5defe52815a56799df
Vaibhav Manni <mannivaibhav95@gmail.com>
2020-10-23 22:25:08 +0530

    made seperate components to dynamically render the homepage

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './homepage.styles.scss'
+
+import Directory from '../../components/directory/directory.component'
+
+export default function Homepage() {
+    return (
+        <div className='homepage'>
+            <Directory/>
+        </div>
+    )
+}
